044c841ef49ded48ce87f01fde4b76bcbaa7de70
Vonglory176 <SkylerGConley@gmail.com>
2024-10-13 21:01:00 -0700

    Added routes for home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <div className="App">
 
       <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+
       <Footer />
       
     </div>
