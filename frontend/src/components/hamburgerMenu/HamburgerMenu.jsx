bdd0c08bea91131e686b33c5b62f921a638081e2
Vonglory176 <SkylerGConley@gmail.com>
2024-10-14 15:22:00 -0700

    Fully functional hamburger menu with animation

---
+++ frontend/src/components/hamburgerMenu/HamburgerMenu.jsx
@@ -0,0 +1,25 @@
+// frontend/src/components/header/HamburgerCross.jsx
+import React from 'react'
+import { FaBars } from 'react-icons/fa'
+// import { useLocation } from 'react-router-dom'
+
+const HamburgerMenu = ({ isOpen, toggleMenu }) => {
+  // const location = useLocation()  
+
+  return (
+    <button className={`hamburger-container h-full lg:hidden p-4 mr-[-16px] ${isOpen ? 'active' : ''}`} onClick={toggleMenu}>
+      
+      
+      {/* <FaBars size={28} /> */}
+
+      <div className={`hamburger-icon duration-300`}>
+        <div className='hamburger-icon-bar1'></div>
+        <div className='hamburger-icon-bar2'></div>
+        <div className='hamburger-icon-bar3'></div>
+      </div>
+
+    </button>
+  )
+}
+
+export default HamburgerMenu
