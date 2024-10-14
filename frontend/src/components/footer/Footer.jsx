0467d5e375ec3f806697a70b23e7a40720d13b5f
Vonglory176 <SkylerGConley@gmail.com>
2024-10-13 21:02:50 -0700

    Footer completed

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,74 +1,94 @@
 import React from 'react'
-import logo from '../../assets/logo.webp'
+import logo from '../../assets/images/logos/logo5.webp'
 import ProgressiveImage from 'react-progressive-graceful-image'
 import { Link } from 'react-router-dom'
 import { FaEnvelope, FaFacebookSquare, FaInstagramSquare, FaMapMarkerAlt, FaPhone, FaYoutubeSquare } from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <footer className='footer'>
+    <footer className='footer flex flex-col gap-10 py-4 px-8 pt-12 sm:px-[8vw]'>
+      
+      {/* Top Container (w/Left and Right) ------------------------------ */}
+      <div className="footer-top grid grid-cols-1 gap-10 sm:grid-cols-2 xl:grid-cols-3">
 
-      {/* Left Side */}
-      <div className="footer-left">
+        {/* Left Side Container ------------------------------ */}
+        <div className="footer-left flex flex-col gap-4">
 
-        {/* Logo */}
-        <Link to={'/'} className='flex h-[80px]'>
-          <ProgressiveImage src={logo} placeholder={''}>
-            {(src, loading) => (
-              <img
-                src={src}
-                alt={'Issaquah Christian Church Logo'}
-                className={`h-full object-contain object-center duration-300 py-2 ${loading ? 'opacity-0' : 'opacity-100'}`}
-              />
-            )}
-          </ProgressiveImage>
-        </Link>
+          {/* Logo */}
+          <Link to={'/'} className='flex h-[80px] w-fit'>
+            <ProgressiveImage src={logo} placeholder={''}>
+              {(src, loading) => (
+                <img
+                  src={src}
+                  alt={'Issaquah Christian Church Logo'}
+                  className={`h-full object-contain object-center duration-300 py-2 ${loading ? 'opacity-0' : 'opacity-100'}`}
+                />
+              )}
+            </ProgressiveImage>
+          </Link>
 
-        {/* Social Media Icons */}
-        <ul className='flex gap-4 text-blue-500'>
-          <li><FaFacebookSquare size={32} /></li>
-          <li><FaInstagramSquare size={32} /></li>
-          <li><FaYoutubeSquare size={32} /></li>
-        </ul>
+          {/* Social Media Icons */}
+          <ul className='flex gap-4'>
+            <li><a href={process.env.REACT_APP_FACEBOOK} target='_blank'><FaFacebookSquare size={32} /></a></li>
+            <li><a href={process.env.REACT_APP_INSTAGRAM} target='_blank'><FaInstagramSquare size={32} /></a></li>
+            <li><a href={process.env.REACT_APP_YOUTUBE} target='_blank'><FaYoutubeSquare size={32} /></a></li>
+          </ul>
+
+        </div>
+
+        {/* Right Side Container ------------------------------ */}
+        <div className="footer-right flex flex-col gap-4">
+          <h2 className='text-2xl font-bold text-white'>Contact Us</h2>
+          <ul className='flex flex-col gap-2'>
+
+            {/* Address */}
+            <li>
+              <a
+                aria-label="view on google maps"
+                href={process.env.REACT_APP_ADDRESS_URL}
+                target='_blank'
+                className='flex items-center gap-2'
+              >
+                <FaMapMarkerAlt size={18} />
+                <p>{process.env.REACT_APP_ADDRESS}</p>
+              </a>
+            </li>
+
+            {/* Phone */}
+            <li>
+              <a
+                aria-label="send a text"
+                href={`tel:${process.env.REACT_APP_PHONE}`}
+                className='flex items-center gap-2'
+              >
+                <FaPhone size={18} />
+                <p>{process.env.REACT_APP_PHONE_FORMATTED}</p>
+              </a>
+            </li>
+
+            {/* Email */}
+            <li>
+              <a
+                aria-label="send an email"
+                href={`mailto:${process.env.REACT_APP_EMAIL}`}
+                className='flex items-center gap-2'
+              >
+                <FaEnvelope size={18} />
+                <p>{process.env.REACT_APP_EMAIL}</p>
+              </a>
+            </li>
+          </ul>
+        </div>
 
       </div>
 
-      {/* Right Side */}
-      <div className="footer-right">
-        <h2>Contact Us</h2>
-        <ul className='flex flex-col gap-2 text-blue-500'>
-          <li>
-            <a
-              aria-label="view on google maps"
-              href="https://maps.app.goo.gl/9SGwb8z93WtezKxv6"
-              target='_blank'
-              className='flex items-center gap-2 hover:text-blue-500 duration-300'
-            >
-              <FaMapMarkerAlt size={18}/>
-              <p>10328 Issaquah-Hobart Rd SE, Issaquah, WA 98027</p>
-            </a>
-          </li>
-          <li>
-            <a
-              aria-label="send a text"
-              href={`tel:${4253925848}`}
-              className='flex items-center gap-2 hover:text-blue-500 duration-300'
-            >
-              <FaPhone size={18}/>
-              <p>(425) 392-5848</p>
-            </a>
-          </li>
-          <li>
-            <a
-              aria-label="send an email"
-              href={`mailto:${'office@issaquah.cc'}`}
-              className='flex items-center gap-2 hover:text-blue-500 duration-300'
-            >
-              <FaEnvelope size={18}/>
-              <p>info@issaqahchristian.org</p>
-            </a>
-          </li>
-        </ul>
+      <hr className='col-span-2' />
+
+      {/* Copyright Container ------------------------------ */}
+      <div className="footer-copyright flex flex-wrap justify-center items-center col-span-2">
+        <p>&copy; {new Date().getFullYear()} Issaquah Christian Church. </p>
+        &nbsp;
+        <p>All rights reserved.</p>
       </div>
     </footer>
   )
