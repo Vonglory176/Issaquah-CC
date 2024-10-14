f5fc1af060ba40c8b7020248262d4b85f329f21f
Vonglory176 <SkylerGConley@gmail.com>
2024-10-13 21:00:52 -0700

    Temp repurposed with FaBars + functionality

--- frontend/src/components/hamburgerCross/HamburgerCross.jsx
+++ frontend/src/components/hamburgerCross/HamburgerCross.jsx
@@ -1,19 +1,22 @@
 // frontend/src/components/header/HamburgerCross.jsx
-import React, { useState } from 'react'
+import React from 'react'
+import { FaBars } from 'react-icons/fa'
+import { useLocation } from 'react-router-dom'
 
-const HamburgerCross = () => {
-  const [isOpen, setIsOpen] = useState(false)
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen)
-  }
+const HamburgerCross = ({ isOpen, toggleMenu }) => {
+  const location = useLocation()  
 
   return (
-    <div className='hamburger-container' onClick={toggleMenu}>
-      <div className={`bar ${isOpen ? 'bar1' : ''}`}></div>
-      <div className={`bar ${isOpen ? 'bar2' : ''}`}></div>
-      <div className={`bar ${isOpen ? 'bar3' : ''}`}></div>
-    </div>
+    <button className={`h-full lg:hidden text-white p-4 mr-[-16px] ${isOpen ? 'active' : ''}`} onClick={toggleMenu}>
+      <FaBars size={28} />
+      {/* <HamburgerCross /> */}
+    </button>
+
+    // <div className='hamburger-container' onClick={toggleMenu}>
+    //   <div className={`bar ${isOpen ? 'bar1' : ''}`}></div>
+    //   <div className={`bar ${isOpen ? 'bar2' : ''}`}></div>
+    //   <div className={`bar ${isOpen ? 'bar3' : ''}`}></div>
+    // </div>
   )
 }
 
