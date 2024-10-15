c716ef7804fb0f003331b65240a459814a65ceda
Vonglory176 <SkylerGConley@gmail.com>
2024-10-14 20:52:12 -0700

    Fixed a bug with hover-toggle working in mobile

--- frontend/src/components/dropdown/Dropdown.jsx
+++ frontend/src/components/dropdown/Dropdown.jsx
@@ -1,22 +1,27 @@
 // frontend/src/components/Dropdown.jsx
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaCaretDown } from 'react-icons/fa'
 
 const Dropdown = ({ children, title, openDropdown, toggleDropdown }) => {
+    const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 1024)
 
-    //   const [isOpen, setIsOpen] = useState(false)
+    // Move to Context later !!! (Currently running for EVERY dropdown)
+    useEffect(() => {
+        const handleResize = () => {
+            setIsDesktop(window.innerWidth >= 1024)
+        }
 
-    //   const toggleDropdown = () => {
-    //     setIsOpen(!isOpen)
-    //   }
+        window.addEventListener('resize', handleResize)
+        return () => window.removeEventListener('resize', handleResize)
+    }, [])
 
     const isOpen = openDropdown === title
 
     return (
         <div 
         className={`dropdown ${isOpen ? 'open' : ''}`} 
-        onMouseEnter={!isOpen ? () => toggleDropdown(title) : null}
-        onMouseLeave={isOpen ? () => toggleDropdown(null) : null}
+        onMouseEnter={isDesktop && !isOpen ? () => toggleDropdown(title) : null}
+        onMouseLeave={isDesktop && isOpen ? () => toggleDropdown(null) : null}
         >
             <button onClick={() => toggleDropdown(title)} className="dropdown-toggle">
                 {title} <FaCaretDown />
