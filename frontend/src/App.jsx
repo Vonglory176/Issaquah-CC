7b9ae759df035d840fc19cb21f63c0f98d7439cc
Vonglory176 <SkylerGConley@gmail.com>
2024-10-14 20:51:43 -0700

    ToDo Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,10 @@ import { Route, Routes } from "react-router-dom";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Home from "./pages/home/Home";
+import Beliefs from "./pages/aboutUs/beliefs/Beliefs";
+import WhatToExpect from "./pages/aboutUs/whatToExpect/WhatToExpect";
+import Vision from "./pages/aboutUs/vision/Vision";
+import Leadership from "./pages/aboutUs/leadership/Leadership";
 
 function App() {
   return (
@@ -13,6 +17,24 @@ function App() {
 
         <Routes>
           <Route path="/" element={<Home />} />
+
+          {/* About Us */}
+          <Route path="/about-us/beliefs" element={<Beliefs />} />
+          <Route path="/about-us/what-to-expect" element={<WhatToExpect />} />
+          <Route path="/about-us/vision-mission-and-values" element={<Vision />} />
+          <Route path="/about-us/leadership" element={<Leadership />} />
+          {/* Event Info */}
+
+          {/* Sermons */}
+
+          {/* Giving */}
+
+          {/* Connect */}
+          
+          {/* Contact */}
+
+          {/* Care */}
+
         </Routes>
 
       </main>
@@ -29,5 +51,13 @@ export default App;
 /* TODO ---------------------------------------
 
   Figure out what to do with Video wrapper
+
+  Set max-width for header/footer/page-wrapper
+
+  Move resize event listener (in Dropdown.jsx) to Context
+
+  Research "Animated banners" for pages
+
+  Figure out what to do for Leadership page
 
 */
