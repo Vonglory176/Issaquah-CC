98a84908d7be072c2c63ca9a4aefd1aa604ce988
Vonglory176 <SkylerGConley@gmail.com>
2024-10-17 17:37:04 -0700

    New files for VideoContainer

---
+++ frontend/src/components/videoContainer/VideoContainer.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import Spinner from '../spinner/Spinner'
+
+const VideoContainer = ({ src, className }) => {
+    const [loading, setLoading] = useState(true)
+
+    return (
+        <div className={`video-container rounded-lg overflow-hidden shadow-lg ${className}`}>
+
+            {loading && <Spinner />}
+
+            <iframe
+                style={{ width: '100%', height: '100%', border: 'none' }}
+                // height="682" // "315"
+                src={src}
+                title="Embedded Video"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                onLoad={() => setLoading(false)}
+            ></iframe>
+        </div>
+    )
+}
+
+export default VideoContainer
