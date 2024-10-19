fe52953293213458b5d5847316693c4f6cea501e
Vonglory176 <SkylerGConley@gmail.com>
2024-10-18 19:12:42 -0700

    New files for Helpful resources page

---
+++ frontend/src/pages/care/helpfulResources/HelpfulResources.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import PageWrapper from '../../../components/pageWrapper/PageWrapper'
+import ProgressiveImage from 'react-progressive-graceful-image'
+import helpingHand from '../../../assets/images/helpingHand.webp'
+import { Link } from 'react-router-dom'
+
+const HelpfulResources = () => {
+    return (
+        <PageWrapper name='helpful-resources' className='flex flex-col gap-12 mx-auto text-center text-lg md:text-xl' maxWidth='max-w-[800px]'>
+
+
+            {/* <hr className='border border-[var(--border-color-3)]' /> */}
+
+            <div className="flex flex-col gap-4">
+                <h1 className='text-3xl font-bold'>We're Here To Help</h1>
+
+                <p>As the church, we are uniquely equipped to bring the light of Jesus to those around us.</p>
+
+                <p>Whether you're in a position of needing help, or you're able to give help, you'll find everything you need to know on this page.</p>
+            </div>
+
+            <div className="helping-hand-image-container rounded-lg overflow-hidden">
+                <ProgressiveImage src={helpingHand} placeholder={''}>
+                    {(src, loading) => (
+                        <img
+                            src={src}
+                            alt={''}
+                            className={`w-full h-full duration-300 ${loading ? 'opacity-0' : 'opacity-100'}`}
+                        />
+                    )}
+                </ProgressiveImage>
+            </div>
+
+            <div className='flex flex-col gap-4 justify-center items-center md:flex-row'>
+                {/* <p>Press the HELP button for various resources.</p> */}
+                {/* <p>Press the donate button to give to the "Mercy Fund".</p> */}
+
+                <Link to='/care/local-resources' className='interactive-button'>
+                    <div>
+                        <p>See Our Resources</p>
+                    </div>
+                </Link>
+
+                <a href='https://giving.servantkeeper.com/issaquahcc' target='_blank' rel='noopener noreferrer' className='interactive-button'>
+                    <div>
+                        <p>Mercy Fund Donation</p>
+                    </div>
+                </a>
+
+            </div>
+
+            <p>For prayer requests, please email the ICC office at <a href="mailto:office@issaquah.cc" className='text-[var(--font-active-color)]'>office@issaquah.cc</a></p>
+
+        </PageWrapper>
+    )
+}
+
+export default HelpfulResources
