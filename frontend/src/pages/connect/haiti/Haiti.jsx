239f85285f3b3398002c733f25a0118f130f0f5e
Vonglory176 <SkylerGConley@gmail.com>
2024-10-18 19:15:31 -0700

    General cleanup

--- frontend/src/pages/connect/haiti/Haiti.jsx
+++ frontend/src/pages/connect/haiti/Haiti.jsx
@@ -11,23 +11,23 @@ const Haiti = () => {
 
                 <h1 className='text-3xl font-bold flex gap-2 flex-wrap mx-auto'>Be Part Of The Solution</h1>
 
+                <div className="flex flex-col gap-8 text-lg md:text-xl text-center">
+                    <p>Click  <a href="https://gvcm.reachapp.co/sponsorships?sponsorship_type=child-sponsorship-orphanage&status=unsponsored,partial" target='_blank' rel='noopener noreferrer' className='text-[var(--font-active-color)]'>Here</a> to Become a Sponsor</p>
+                </div>
 
                 {/* <p className='text-lg md:text-xl'>We have a three step process to become a member at Issaquah Christian Church:</p> */}
 
-                <hr className='border border-[var(--border-color-3)]' />
+                {/* <hr className='border border-[var(--border-color-3)]' /> */}
 
             </section>
 
-            <div className="flex flex-col gap-8 text-lg md:text-xl text-center">
-                <p>Click  <a href="https://gvcm.reachapp.co/sponsorships?sponsorship_type=child-sponsorship-orphanage&status=unsponsored,partial" target='_blank' rel='noopener noreferrer' className='text-[var(--font-active-color)]'>Here</a> to Become a Sponsor</p>
-            </div>
 
             <ProgressiveImage src={sponsor} placeholder={''}>
                 {(src, loading) => (
                     <img
                         src={src}
-                        alt={'Staff Member'}
-                        className={`w-full max-w-[500px] h-full mx-auto duration-300 ${loading ? 'opacity-0' : 'opacity-100'}`}
+                        alt={''}
+                        className={`w-full max-w-[500px] h-full mx-auto duration-300 rounded-lg ${loading ? 'opacity-0' : 'opacity-100'}`}
                     />
                 )}
             </ProgressiveImage>
