0beaef86263080730461f963f09be7bbf9172904
Vonglory176 <SkylerGConley@gmail.com>
2024-10-20 21:25:22 -0700

    Added Banner use logic

--- frontend/src/components/pageWrapper/PageWrapper.jsx
+++ frontend/src/components/pageWrapper/PageWrapper.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
+import Banner from '../banner/Banner'
 
 // This component is used to wrap pages and provide a background image (If given).
-const PageWrapper = ({ children, name='', className='', maxWidth='max-w-[1090px]', backgroundImages={ // 
-    large: null, 
-    small: null
-} }) => {
+const PageWrapper = ({ children, name = '', className = '', maxWidth = 'max-w-[1090px]', backgroundSettings = { image:{ large: null, small: null }}, bannerSettings }) => {
 
   return (
     <div className={`page-wrapper ${name}-wrapper`}>
+      
+      {/* Banner */}
+      {bannerSettings && <Banner bannerSettings={bannerSettings} />}
 
-        <div className={`page-content ${name ? name + '-content' : ''} ${className} ${maxWidth} `}> {/* */}
-            {children}
-        </div>
+      {/* Page Content */}
+      <div className={`page-content ${name ? name + '-content' : ''} ${className} ${maxWidth} `}> {/* */}
+        {children}
+      </div>
 
     </div>
   )
