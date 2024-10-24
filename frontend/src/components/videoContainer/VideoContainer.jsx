0758e0c0ae67e95d70fd4f535046d2fec229a580
Vonglory176 <SkylerGConley@gmail.com>
2024-10-23 19:09:08 -0700

    Added "loading=lazy"

--- frontend/src/components/videoContainer/VideoContainer.jsx
+++ frontend/src/components/videoContainer/VideoContainer.jsx
@@ -9,6 +9,18 @@ const VideoContainer = ({ src, className }) => {
 
             {loading && <Spinner />}
 
+            {/* <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0, overflow: 'hidden' }}>
+                <iframe
+                    style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', border: 'none' }}
+                    src={src}
+                    title="Embedded Video"
+                    allow="autoplay; clipboard-write; encrypted-media; picture-in-picture" // accelerometer;  gyroscope;
+                    allowFullScreen
+                    loading="lazy"
+                    onLoad={() => setLoading(false)}
+                ></iframe>
+            </div> */}
+
             <iframe
                 style={{ width: '100%', height: '100%', border: 'none' }}
                 // height="682" // "315"
@@ -16,6 +28,7 @@ const VideoContainer = ({ src, className }) => {
                 title="Embedded Video"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
+                loading="lazy"
                 onLoad={() => setLoading(false)}
             ></iframe>
         </div>
