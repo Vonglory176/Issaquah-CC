a6ed9ddb7ce75f86b6cf197e6f12ce9e58af56e3
Vonglory176 <SkylerGConley@gmail.com>
2024-10-25 13:26:30 -0700

    General cleanup

--- frontend/src/components/videoContainer/VideoContainer.jsx
+++ frontend/src/components/videoContainer/VideoContainer.jsx
@@ -9,21 +9,9 @@ const VideoContainer = ({ src, className }) => {
 
             {loading && <Spinner />}
 
-            {/* <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0, overflow: 'hidden' }}>
-                <iframe
-                    style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', border: 'none' }}
-                    src={src}
-                    title="Embedded Video"
-                    allow="autoplay; clipboard-write; encrypted-media; picture-in-picture" // accelerometer;  gyroscope;
-                    allowFullScreen
-                    loading="lazy"
-                    onLoad={() => setLoading(false)}
-                ></iframe>
-            </div> */}
-
             <iframe
                 style={{ width: '100%', height: '100%', border: 'none' }}
-                // height="682" // "315"
+                //   height="682"  "315"
                 src={src}
                 title="Embedded Video"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -31,6 +19,7 @@ const VideoContainer = ({ src, className }) => {
                 loading="lazy"
                 onLoad={() => setLoading(false)}
             ></iframe>
+
         </div>
     )
 }
