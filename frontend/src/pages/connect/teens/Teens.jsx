57a805e4fe58d1aed0de2adcd1c9f9311a2a110a
Vonglory176 <SkylerGConley@gmail.com>
2024-10-25 18:39:29 -0700

    Added aria/titles to links & a loading animation to slideshow

--- frontend/src/pages/connect/teens/Teens.jsx
+++ frontend/src/pages/connect/teens/Teens.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageWrapper from '../../../components/pageWrapper/PageWrapper'
 import ProgressiveImage from 'react-progressive-graceful-image'
+import Spinner from '../../../components/spinner/Spinner'
 
 import youthGroup from '../../../assets/images/youthGroup.webp'
 import dcVsMarvel from '../../../assets/images/dcVsMarvel.webp'
@@ -8,6 +9,8 @@ import dcVsMarvel from '../../../assets/images/dcVsMarvel.webp'
 
 
 const Teens = () => {
+    const [slideshowLoaded, setSlideshowLoaded] = useState(false)
+
     return (
         <PageWrapper name='teens' className='flex flex-col gap-8 text-center' maxWidth='max-w-[800px]' bannerSettings={{
             text: {
@@ -28,23 +31,23 @@ const Teens = () => {
 
             <div className="flex flex-col gap-8">
                 <p>Grades 6-12 meet on Wednesday Night at 7pm at Pastor Aaron's House. We eat dinner together and study the Bible together.</p>
-                <p>Email <a href="mailto:aaron@issaquah.cc" target='_blank' rel='noopener noreferrer' className='text-[var(--font-active-color-1)]'>aaron@issaquah.cc</a> to stay updated on our activities.</p>
+                <p>Email <a href="mailto:aaron@issaquah.cc" target='_blank' rel='noopener noreferrer' aria-label="Email Pastor Aaron" title="Email Pastor Aaron" >aaron@issaquah.cc</a> to stay updated on our activities.</p>
             </div>
 
             <div>
                 <div className="slideshow-container shadow-md rounded-lg overflow-hidden bg-white" style={{ width: '100%', position: 'relative', margin: '20px auto', paddingBottom: '75%' }}>
 
-                    {/* {!playerLoaded && <Spinner />} */}
+                    {!slideshowLoaded && <Spinner />}
 
                     <iframe
                         title="Slideshow"
                         src={`https://www.canva.com/design/DADyJ2I9HMM/view?embed`}
                         aria-label="Slideshow"
                         style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', border: '0' }}
-                    // onLoad={() => setPlayerLoaded(true)}
+                        onLoad={() => setSlideshowLoaded(true)}
                     />
                 </div>
-                <p><a href="https://www.canva.com/design/DADyJ2I9HMM/view?utm_content=DADyJ2I9HMM&utm_campaign=designshare&utm_medium=embeds&utm_source=link" target='_blank' rel='noopener noreferrer'>Winter Teen Retreat - January 2020</a> by <a href="https://www.canva.com/aaron3124?utm_campaign=designshare&utm_medium=embeds&utm_source=link">Aaron Bauer</a></p>
+                <p><a href="https://www.canva.com/design/DADyJ2I9HMM/view?utm_content=DADyJ2I9HMM&utm_campaign=designshare&utm_medium=embeds&utm_source=link" target='_blank' rel='noopener noreferrer' aria-label="View the slideshow on Canva" title='View the slideshow on Canva' >Winter Teen Retreat - January 2020</a> by <a href="https://www.canva.com/aaron3124?utm_campaign=designshare&utm_medium=embeds&utm_source=link" aria-label="Visit Aaron Bauer's profile on Canva" title="Visit Aaron Bauer's profile on Canva" >Aaron Bauer</a></p>
             </div>
 
             <ProgressiveImage src={youthGroup} placeholder={''}>
