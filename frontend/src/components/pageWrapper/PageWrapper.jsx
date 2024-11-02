e6508f178bee012cc3c943386928c9ba660a6879
Vonglory176 <SkylerGConley@gmail.com>
2024-11-01 17:19:34 -0700

    Added SEO component

--- frontend/src/components/pageWrapper/PageWrapper.jsx
+++ frontend/src/components/pageWrapper/PageWrapper.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import Banner from '../banner/Banner'
+import SEO from '../seo/SEO'
 
 // This component is used to wrap pages and provide a background image (If given).
-const PageWrapper = ({ children, name = '', className = '', maxWidth = 'max-w-[1090px]', backgroundSettings = { image:{ large: null, small: null }}, bannerSettings }) => {
+const PageWrapper = ({ children, name = '', className = '', maxWidth = 'max-w-[1090px]', bannerSettings, metaSettings, backgroundSettings, }) => {
 
   return (
     <div className={`page-wrapper ${name}-wrapper`}>
+
+      {/* Meta */}
+      <SEO {...metaSettings} />
       
       {/* Banner */}
-      {bannerSettings && <Banner bannerSettings={bannerSettings} />}
+      {bannerSettings && <Banner {...bannerSettings} />}
 
       {/* Page Content */}
       <div className={`page-content ${name ? name + '-content' : ''} ${className} ${maxWidth} `}> {/* */}
