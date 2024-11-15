5829630b716233bfb302e36958d119f2eb2f4b18
Vonglory176 <SkylerGConley@gmail.com>
2024-11-14 16:47:52 -0800

    "Unregistered" the service worker

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,30 +9,30 @@ import { HelmetProvider } from 'react-helmet-async'
 // import reportWebVitals from './reportWebVitals'
 
 // Register service worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(registration => {
-        console.log('Service Worker registered with scope: ', registration.scope)
-      })
-      .catch(error => {
-        console.error('Service Worker registration failed: ', error)
-      })
-  })
-}
-
-// Unregister service worker
 // if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker.getRegistrations().then(registrations => {
-//     for (const registration of registrations) {
-//       registration.unregister().then(() => {
-//         console.log('Service Worker unregistered')
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('/service-worker.js')
+//       .then(registration => {
+//         console.log('Service Worker registered with scope: ', registration.scope)
 //       })
-//     }
-//   }).catch(error => {
-//     console.error('Error unregistering service workers:', error)
+//       .catch(error => {
+//         console.error('Service Worker registration failed: ', error)
+//       })
 //   })
 // }
+
+// Unregister service worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(registrations => {
+    for (const registration of registrations) {
+      registration.unregister().then(() => {
+        console.log('Service Worker unregistered')
+      })
+    }
+  }).catch(error => {
+    console.error('Error unregistering service workers:', error)
+  })
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
